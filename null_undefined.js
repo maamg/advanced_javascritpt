cce75f53b8a890e13c0f98fad3fa13e083ec2220
maamg <maamg164@gmail.com>
2021-02-14 20:08:10 +0600

    null vs undefined

---
+++ null_undefined.js
@@ -0,0 +1,21 @@
+let pakhi;
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return ;
+}
+
+function add(num1, num2){
+    console.log(num1);
+}
+
+
+let pemikPurush = { name: 'Akash', phone: 2909, address: 'aldjfa'};
+
+let fun = undefined;
+
+let nums = [13,24,63,24];
+
+let nums = null;
+
+console.log(nums);
