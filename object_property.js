2a093a3f5fc3c13d1738b96d7b9ac6c5a300c1fc
maamg <maamg164@gmail.com>
2021-02-15 15:05:43 +0600

    appling map and soOn

--- object_property.js
+++ object_property.js
@@ -1,14 +1,21 @@
 const student = [
-    {id: 001, name:'Ahsan'},
-    {id: 002, name:'Ahnaf'},
-    {id: 003, name:'Ahbab'},
-    {id: 002, name:'sakib'},
-    {id: 002, name:'shahed'}
+    {id: 21, name:'Ahsan'},
+    {id: 32, name:'Ahnaf'},
+    {id: 43, name:'Ahbab'},
+    {id: 52, name:'sakib'},
+    {id: 62, name:'shahed'}
 ]
 
-const sList =[];
 
-for (let i = 0; i < student.length; i++) {
-    sList.push(student[i].name);  
-}
-console.log(sList);
+//...........>traditional looping method<.......
+// const sList =[];
+// for (let i = 0; i < student.length; i++) {
+//     sList.push(student[i].name);  
+// }
+// console.log(sList);
+
+
+// .......<map method>.........
+// console.log(student.map(x => x.name));
+// console.log(student.map(x => x.id));
+console.log(student.find(x => x.id > 42));
